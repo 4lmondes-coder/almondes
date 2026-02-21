54ecccca50c350c5286bb0f81949a69e77cce8f3
4lmondes-coder <4lmondes@gmail.com>
2026-02-21 01:51:17 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,34 +3,20 @@ const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './auto-play.js', // se tiver script separado
-  './player.js',    // se tiver script separado
-  'https://cdn.jsdelivr.net/npm/jsmediatags@3.9.7/dist/jsmediatags.min.js',
-  // adicione aqui outros arquivos estáticos que queira offline
+  'https://cdn.jsdelivr.net/npm/jsmediatags@3.9.7/dist/jsmediatags.min.js'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('[ServiceWorker] Cacheando arquivos');
-        return cache.addAll(FILES_TO_CACHE);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removendo cache antigo', key);
-            return caches.delete(key);
-          }
-        })
-      )
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
     )
   );
   self.clients.claim();
@@ -38,21 +24,11 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request)
-          .then((res) => {
-            return caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, res.clone());
-              return res;
-            });
-          });
-      })
-      .catch(() => {
-        // fallback opcional se estiver offline
-        if (event.request.destination === 'document') {
-          return caches.match('./index.html');
-        }
-      })
+    caches.match(event.request).then(resp => resp || fetch(event.request).then(res => {
+      return caches.open(CACHE_NAME).then(cache => {
+        cache.put(event.request, res.clone());
+        return res;
+      });
+    })).catch(() => event.request.destination === 'document' ? caches.match('./index.html') : null)
   );
 });
